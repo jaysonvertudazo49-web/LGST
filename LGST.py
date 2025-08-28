d62c856b88f7025fa6eebd5566a9efcaad708234
jaysonvertudazo49-web <jaysonvertudazo49@gmail.com>
2025-08-28 09:19:37 +0800

    Update LGST.py

--- LGST.py
+++ LGST.py
@@ -39,7 +39,7 @@ body {
     align-items: center;
 }
 .header-title img {
-    margin-right: 10px;
+    margin-right: 50px;
 }
 .header-title h1 { 
     margin: 0; 
@@ -559,6 +559,7 @@ elif st.session_state.page == "Admin":
 
 # ------------------ FOOTER ------------------
 st.markdown("""<div class="footer">© 2025 Lucas Grey Scrap Trading. All rights reserved.</div>""", unsafe_allow_html=True)
+
 
 
 
