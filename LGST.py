fc5da1c4e1d25ad78f6782f89232a85f37625473
jaysonvertudazo49-web <jaysonvertudazo49@gmail.com>
2025-08-28 15:29:03 +0800

    Update LGST.py

--- LGST.py
+++ LGST.py
@@ -627,7 +627,9 @@ elif st.session_state.page == "Admin":
                 st.session_state.is_admin = False; st.rerun()
 
 
-    # ------------------ FOOTER ------------------ st.markdown("""<div class="footer">© 2025 Lucas Grey Scrap Trading. All rights reserved.</div>""", unsafe_allow_html=True)
+    # ------------------ FOOTER ------------------ 
+    st.markdown("""<div class="footer">© 2025 Lucas Grey Scrap Trading. All rights reserved.</div>""", unsafe_allow_html=True)
+
 
 
 
